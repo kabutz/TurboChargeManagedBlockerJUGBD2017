b42dd403d3e951921e0d706c5969e4bf932794d5
kabutz <heinz@javaspecialists.eu>
2017-09-09 20:21:25 +0300

    step 4

--- forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
+++ forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
@@ -1,41 +1,52 @@
 package eu.javaspecialists.performance.managedblocker;
 
 import java.math.*;
+import java.util.*;
 import java.util.concurrent.*;
 
 // 1. test100_000_000() time = 55152
 // 2. test100_000_000() time = 28813
 // 3. test100_000_000() time = 17573
-
+// 4. test100_000_000() time = 12742
 // TODO: Sign up to Heinz's Newsletter: www.javaspecialists.eu
 public class Fibonacci {
     public BigInteger f(int n) {
-        if (n == 0) return BigInteger.ZERO;
-        if (n == 1) return BigInteger.ONE;
+        Map<Integer, BigInteger> cache = new ConcurrentHashMap<>();
+        cache.put(0, BigInteger.ZERO);
+        cache.put(1, BigInteger.ONE);
 
-        int half = (n + 1) / 2;
+        return f(n, cache);
+    }
 
-        RecursiveTask<BigInteger> f0_task = new RecursiveTask<BigInteger>() {
-            protected BigInteger compute() {
-                return f(half-1);
-            }
-        };
-        f0_task.fork();
-        BigInteger f1 = f(half);
-        BigInteger f0 = f0_task.join();
+    private BigInteger f(int n, Map<Integer, BigInteger> cache) {
+        BigInteger result = cache.get(n);
+        if (result == null) {
+            int half = (n + 1) / 2;
 
-        long time = n > 1000 ? System.currentTimeMillis() : 0;
-        try {
-            if (n % 2 == 1) {
-                return f0.multiply(f0).add(f1.multiply(f1));
-            } else {
-                return f0.shiftLeft(1).add(f1).multiply(f1);
-            }
-        } finally {
-            time = n > 1000 ? System.currentTimeMillis() - time : 0;
-            if (time > 50) {
-                System.out.printf("f(%d) took %d%n", n, time);
+            RecursiveTask<BigInteger> f0_task = new RecursiveTask<BigInteger>() {
+                protected BigInteger compute() {
+                    return f(half - 1, cache);
+                }
+            };
+            f0_task.fork();
+            BigInteger f1 = f(half, cache);
+            BigInteger f0 = f0_task.join();
+
+            long time = n > 1000 ? System.currentTimeMillis() : 0;
+            try {
+                if (n % 2 == 1) {
+                    result = f0.multiply(f0).add(f1.multiply(f1));
+                } else {
+                    result = f0.shiftLeft(1).add(f1).multiply(f1);
+                }
+                cache.put(n, result);
+            } finally {
+                time = n > 1000 ? System.currentTimeMillis() - time : 0;
+                if (time > 50) {
+                    System.out.printf("f(%d) took %d%n", n, time);
+                }
             }
         }
+        return result;
     }
 }
